7109d1fdd957a073277485e13442dd591143c4e5
Namima1 <129132573+Namima1@users.noreply.github.com>
2023-07-06 17:54:51 -0400

    Add files via upload

---
+++ boolean_practice.rb
@@ -0,0 +1,21 @@
+#boolean refers to a value that's either true or false. 
+
+puts true 
+puts false 
+
+puts 10 < 15 
+puts 10 > 15 
+
+#< expression asks if 10 is less than 15, ruby will give a true
+#> expression asks if 10 is greater than 15, ruby will give a false
+
+Great = true 
+Bad = false 
+
+#Great is the object that is assigned to true 
+#bad is the object assigned to false 
+
+puts Great.class
+puts Bad.class 
+
+#Assigned class method because every object has a class
