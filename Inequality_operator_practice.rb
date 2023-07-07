cfe084cc64a2f0c7053decc81e83580587117929
Namima1 <129132573+Namima1@users.noreply.github.com>
2023-07-06 20:13:00 -0400

    Add files via upload

---
+++ Inequality_operator_practice.rb
@@ -0,0 +1,12 @@
+#Inequality operator !=
+#checks if two values are not equal to one another 
+
+puts "Hi" != "Hello"
+puts "Today" != "Day"
+puts "Food" != "cuisine"
+
+#Results for all will be true because they are not equal to each other
+
+puts "Hi" != "Hi"
+
+#result will be false bc they are equal to each other 
