de59f4ade5016c9512145ba141a29c70ab9fd826
Namima1 <129132573+Namima1@users.noreply.github.com>
2023-07-06 21:23:22 -0400

    Add files via upload

---
+++ float_method.rb
@@ -0,0 +1,20 @@
+#float is a number with decimal 
+#float always rounds down 
+
+puts 12.5.floor 
+#result will be 12, floor rounds down 
+
+puts 12.99.floor 
+#result will be 12
+
+puts 12.5.ceil 
+#result is 13, ceil rounds up
+
+puts 3.14159.round 
+#round gives the closest integer 
+
+puts 12.99
+#result is 13
+
+puts 3.14159.round(2)
+#rounds the number 2 spaces after the decimal
