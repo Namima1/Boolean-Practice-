f669893cdc3009ce6cc9d8b6fbe3416bb86ef99c
Namima1 <129132573+Namima1@users.noreply.github.com>
2023-07-06 20:41:24 -0400

    Add files via upload

---
+++ predicate_method_practice.rb
@@ -0,0 +1,14 @@
+#predicate methods are written like a question 
+
+puts 12.odd? 
+#result will be false bc it is asking if 12 is odd, 12 is a even number
+
+puts 11.odd? 
+#result will be true 
+
+puts 3.even? 
+#result will be false 
+
+puts 4.even? 
+#result will be true 
+
